16af41211d80b4124a0fafe84c3109bfe60a9381
geekbrains <info@geekbrains.ru>
2023-01-22 04:21:04 +0300

    Lesson 3

--- app/src/main/java/com/stellarworker/car/MainActivity.kt
+++ app/src/main/java/com/stellarworker/car/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.stellarworker.car
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.ArcMotion
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
+import com.stellarworker.car.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainActivityCar.setOnClickListener { car ->
+            val changeBounds = ChangeBounds()
+            changeBounds.setPathMotion(ArcMotion())
+            changeBounds.duration = ANIMATION_DURATION
+            TransitionManager.beginDelayedTransition(
+                binding.mainActivityRoot,
+                changeBounds
+            )
+            val params = car.layoutParams as FrameLayout.LayoutParams
+            params.gravity = Gravity.END or Gravity.BOTTOM
+            car.layoutParams = params
+        }
+    }
+
+    companion object {
+        private const val ANIMATION_DURATION = 2000L
     }
 }
